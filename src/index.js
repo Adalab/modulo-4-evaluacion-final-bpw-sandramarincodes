80e74e92dad16bb4bc88485034c322a227144fa7
sandramarincodes <smarinfernan@gmail.com>
2025-08-02 15:05:17 +0200

    Update get personajes

--- src/index.js
+++ src/index.js
@@ -145,3 +145,16 @@ server.post("/frases", async (req, res) => {
     res.status(500).json({ error: error });
   }
 });
+
+// Listar todos los personajes
+server.get("/personajes", async (req, res) => {
+  try {
+    const conn = await getConnection();
+    const [result] = await conn.query("SELECT * FROM personajes");
+
+    await conn.end();
+    res.json(result);
+  } catch (error) {
+    res.status(500).json({ error: "Error al obtener los personajes." });
+  }
+});
